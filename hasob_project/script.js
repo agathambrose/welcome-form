c3b26407748494b473c08dbdda81e475d01dd825
Agatha Ambrose <agathambrose@gmail.com>
2020-12-27 22:57:41 +0100

    Json attempt

--- hasob_project/script.js
+++ hasob_project/script.js
@@ -1,38 +1,11 @@
 
-/*Start JSON*/
-/*
-    async function submitForm(e, form){
-        //prevent page reload
-        e.preventDefault();
-        //submit form
-
-        //user interaction
-        const submitBtn = document.getElementById("submitBtn");
-        submitBtn.disabled = true;
-        setTimeout(() => submitBtn.disabled = false, 2000);
-        //JSON body
-        const jsonFormData = buildJsonFormData(form);
-        //JSON headers
-        const headers = buildHeaders();
-        //request and response
-        const response = await fetchService.performPostHttpRequest('https://jsonplaceholder.typicode.com/posts', headers, jsonFormData); //makes use of JSON placeholder
-        console.log(response); //informs user of result
-
-        if(response)
-            window.location = `/success.html?username=${response.username}&radio=${response.radio}&dropDwn=${response.dropDwn}&checkboxes=${response.checkboxes}&email=${response.email}&id=${response.id}`;
-        else
-            alert(`An error occurred`)
-    }
-/*End JSON*/
-
-
+/*Start JQuery Form Validation*/
 $(document).ready(()=>{
     $("#form").on('submit',  ()=>{
     ValidateForm();
     return false;
-});
+    });
 })
-
 
 function EmailValidate(){
     var numericExpression = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -51,7 +24,6 @@ function ValidateForm(){
     let errorEmail = "*Email required"
 
     //Radios
-
     if ($('input[name= "gender"]:checked').length == 0) {
         $("#errorRadios").html(errorRadios);
         errorCounter++;
@@ -75,7 +47,6 @@ function ValidateForm(){
     //End
 
     //Checkboxes
-
     if ($('input[type = checkbox]:checked').length == 0)
     {$("#errorCheckbox").html(errorCheckbox);
         errorCounter++;
@@ -83,7 +54,6 @@ function ValidateForm(){
     else{
         $("#errorCheckbox").html("");
     }
-
     //End
 
     //Email
@@ -99,12 +69,111 @@ function ValidateForm(){
     }
     //End  
    
+    /*
     if (errorCounter == 0) {
         alert('Registration Successful!');
         return true;
     }else {
         return false;
-    }
+    } */
 }
-    
+/*End JQuery Form Validation*/
 
+/*Form to JSON*/
+/*
+const formIsValid = (element) => {
+    return element.name && element.value
+  }
+
+  const formIsChecked = element => {
+    return !['checkbox', 'radio'].includes(element.type) || element.checked
+  }
+
+  const formHasCheckBox = element => element.type === "checkbox";
+  
+  const takeFormToJSON = elements => {
+    const reducerFunction = (data, element) => {
+      data[element.name] = element.value;
+      return data
+    }
+    const reducerInitialValue = {};
+    const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
+    return formData
+  }
+
+  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    if (formIsValid(element) && formIsChecked(element)) {
+      if (formHasCheckBox(element)) {
+        data[element.name] = (data[element.name] || []).concat(element.value)
+      }
+      else data[element.name] = element.value
+    }
+    return data;
+  }, {})
+
+  const onSubmit = document.getElementById('form');
+  
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    const data = formToJSON(form.elements);
+    const dataDisplay = document.querySelectorAll(".prompt-display")[0];
+    dataDisplay.textContent = JSON.stringify(data, form, null, " ").serializeArray;
+  }
+*/
+/*
+const handleFormSubmit = event => {
+    event.preventDefault();
+    const data = {};
+    const dataContainer = document.getElementsByClassName('prompt_display')[0];
+    dataContainer.textContent = JSON.stringify(data, null, "  ");
+};
+
+const form = document.getElementsByClassName('form')[0];
+form.addEventListener('submit', handleFormSubmit);
+
+const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    data[element.name] = element.value;
+    return data;
+  }, {});
+
+const handleFormSubmit = event => {
+    event.preventDefault();
+    const data = formToJSON(form.elements);
+    const dataContainer = document.getElementsByClassName('prompt_display')[0];
+    dataContainer.textContent = JSON.stringify(data, null, "  ");
+  const form = document.getElementsByClassName('form')[0];
+  form.addEventListener('submit', handleFormSubmit);
+
+  const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    data[element.name] = element.value;
+    return data;
+  }, {});
+
+const handleFormSubmit = event => {
+    event.preventDefault();
+    const data = formToJSON(form.elements);
+    const dataContainer = document.getElementsByClassName('prompt_display')[0];
+    dataContainer.textContent = JSON.stringify(data, null, "  ");
+};
+
+const form = document.getElementsByClassName('form')[0];
+form.addEventListener('submit', handleFormSubmit);*/
+
+let data = [];
+
+const formData = (ev) => {
+    ev.preventDefault();
+    let allData = {
+        form: document.getElementById('form').value
+    }
+    data.push(allData);
+    document.forms[0].reset();
+
+    console.warn('submit', {data});
+    let div = document.querySelector('#prompt');
+    div.textContent = '\n' + JSON.stringify(data, '\t');
+}
+
+document.getElementById('submitBtn').addEventListener('submit', formData)
